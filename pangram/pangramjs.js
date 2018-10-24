e814b3939609228408da6e7aa7320c0e60adb49d
phathraphon <phathraphon.pandey@successive.tech>
2018-10-24 19:04:28 +0530

    added string assignment

--- pangram/pangramjs.js
+++ pangram/pangramjs.js
@@ -1,5 +1,6 @@
 function func() {
     let str = document.getElementById("inpt").value;
+    console.log(str);
     str = str.toLowerCase();
     let alp = "qwertyuiopasdfghjklzxcvbnm";
     let len = str.length;
@@ -8,7 +9,7 @@ function func() {
         for (let i = 0; i < 26; i++) {
             c = 0;
             for (let j = 0; j < len; j++) {
-                if(alp.charAt(i)==str.charAt(j)||alp.charAt(i)==" "){
+                if(alp.charAt(i)==str.charAt(j)){
                     c=1;
                     break;
                 }
@@ -21,4 +22,22 @@ function func() {
     document.getElementById("result").innerHTML="Pangram";
     else
     document.getElementById("result").innerHTML="Not Pangram";
-}       
+}  
+
+
+function fun() {
+    let str = document.getElementById("txt").value;
+    console.log(str);
+    let len = str.length;
+    let count = 0;
+    let prev = str.charAt(0);
+    console.log(prev);
+    for (let a = 1; a < len; a++) {
+        if (prev == str.charAt(a)) {
+            count += 1;
+            continue;
+        }
+        prev = str.charAt(a);
+    }
+    document.getElementById("res").innerHTML=count;
+}
